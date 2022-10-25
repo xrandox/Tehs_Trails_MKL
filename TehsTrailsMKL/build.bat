b358e7ce61dec317ccf42d02dd48d68c2316941f
xrandox <xrandox17@yahoo.com>
2022-10-25 00:45:43 -0500

    initial upload

---
+++ TehsTrailsMKL/build.bat
@@ -0,0 +1,5 @@
+del ".\TehsTrailsMKL.taco"
+
+"%ProgramFiles%\WinRAR\WinRAR.exe" a -afzip -ep1 -ibck -r -y ".\TehsTrailsMKL.zip" ".\Data" ".\TehsTrailsMKL.xml"
+
+rename "TehsTrailsMKL.zip" "TehsTrailsMKL.taco"
